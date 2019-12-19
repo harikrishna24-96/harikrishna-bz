f4b56cc264dfc09e898290612c8715220c880409
harikrishna24-96 <harikrishna.todupunuri@gmail.com>
2019-12-19 16:29:01 +0530

    API calling json

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ const port = process.env.PORT || 3000
 app.use(express.static('public'))
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/index.html'))
+var obj={}
+obj.name ="Being zero";
+obj.college = "CMRCET";
+obj.rno = "17H51A05H6";
+app.get('/data',(req,res)=> res.json(obj))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
